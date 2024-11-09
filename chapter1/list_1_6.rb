2e05b4f6fca283ded0ef973e89ca4d84e06d879d
shion0104 <zhiyinzhongze@gmail.com>
2024-11-09 14:21:37 +0900

    チャプター1終了

---
+++ chapter1/list_1_6.rb
@@ -0,0 +1,14 @@
+# クラスやモジュールの名称は単語の始まりを大文字で始める
+class SampleClass
+end
+
+module SampleModule
+end
+
+# 真偽値を表すメソッドはis_xxx?のように表現されることが多い
+def happy?
+    true
+end
+
+# 変数名は_でつなげたスネークケースで記述するのが慣例となっている
+sample_variable = 'sample'
